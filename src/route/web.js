d400e5ae66517d374fbe57a0e8e73369e208da8a
k1v4z <addnguyen32gmail.com>
2024-02-20 21:41:40 +0700

    route

---
+++ src/route/web.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+  
+router.get('/viewengine', (req, res) => {
+  res.render('sample.ejs');
+})
+
+module.exports = router;
