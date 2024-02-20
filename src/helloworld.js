f138744064f1763999210ac60cf0595480ab8803
k1v4z <addnguyen32gmail.com>
2024-02-20 14:24:16 +0700

    view engine

---
+++ src/helloworld.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path =  require('path')
+const app = express()
+const port = 8081
+//config template engine
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/viewengine', (req, res) => {
+  res.render('sample.ejs');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
