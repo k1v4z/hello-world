505d20c5fe2166f569131641dab1594404ff9bea
k1v4z <addnguyen32gmail.com>
2024-02-21 22:48:15 +0700

    config promise

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,16 @@
 
 require('dotenv').config();
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 // Create the connection to database
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    connectionLimit: 10,
+    waitForConnections: true
 });
 
 module.exports = connection;
