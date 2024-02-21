34d00f2ebcb284aa18bf100d5cd6d713fc47d90e
k1v4z <addnguyen32gmail.com>
2024-02-21 08:44:02 +0700

    split connection into config file

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const path =  require('path');
-const configViewEngine = require('./config/ViewEngine');
-const webRouter = require('./route/web');
 require('dotenv').config();
 
+const express = require('express');
+const configViewEngine = require('./config/ViewEngine');
+const webRouter = require('./route/web');
+const connection = require('./config/db')
 const app = express();
 const port = process.env.PORT;
 const localhost = process.env.HOST_NAME;
 
 //config template engine and static file
 configViewEngine(app);
+
+connection.query(
+  'SELECT * FROM `Users`',
+  function (err, results, fields) {
+    console.log(">>> results: ", results); // results contains rows returned by server
+  }
+);
 
 //route
 app.use('/',webRouter);
