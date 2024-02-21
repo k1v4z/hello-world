8d03ca1b8f22505fde8593fa95027416ce3b1e2d
k1v4z <addnguyen32gmail.com>
2024-02-21 17:14:28 +0700

    comment test connection

--- src/server.js
+++ src/server.js
@@ -13,12 +13,12 @@ const localhost = process.env.HOST_NAME;
 //config template engine and static file
 configViewEngine(app);
 
-connection.query(
-  'SELECT * FROM `Users`',
-  function (err, results, fields) {
-    console.log(">>> results: ", results); // results contains rows returned by server
-  }
-);
+// connection.query(
+//   'SELECT * FROM `Users`',
+//   function (err, results, fields) {
+//     console.log(">>> results: ", results); // results contains rows returned by server
+//   }
+// );
 
 //config req.body
 app.use(bodyParser.json());
