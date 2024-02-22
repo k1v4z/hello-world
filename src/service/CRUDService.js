c0484ee49fdeca1b7ac2b113ba0d1b9c658409f2
k1v4z <addnguyen32gmail.com>
2024-02-22 23:03:45 +0700

    add getUserById

--- src/service/CRUDService.js
+++ src/service/CRUDService.js
@@ -1,21 +1,27 @@
-const connection =  require('../config/db')
+const connection = require('../config/db')
 
-const getAllUser = async () =>{
+const getAllUser = async () => {
     let [results, fields] = await connection.query('SELECT * FROM Users');
     return results;
 }
 
 const addUser = async (req) => {
-    let {name, email, city} = req.body;
+    let { name, email, city } = req.body;
 
     const [results, fields] = await connection.query(
-    `INSERT INTO 
+        `INSERT INTO 
     Users (email, name, city)
     VALUES (?,?,?);`,
-    [email, name, city]);
-    
+        [email, name, city]);
+
     return results;
 }
+const getUserById = async (userId) => {
+    const [results] = await connection.query('SELECT * FROM Users WHERE id = ?', [userId]);
+    let user = results && results.length > 0 ? results[0] : {};
+
+    return user;
+}
 module.exports = {
-    getAllUser, addUser
+    getAllUser, addUser, getUserById
 }
