27aed7a3d0e3b049525727b16e3447cd36f3c7d6
k1v4z <addnguyen32gmail.com>
2024-02-22 23:00:55 +0700

    add route

--- src/route/web.js
+++ src/route/web.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {postAddUser, getHomePage, getCreateUser} = require('../controller/HomeController');
+const { postAddUser, getHomePage, getCreateUser, editUser } = require('../controller/HomeController');
 
 //router.Method(route,handler);
 router.get('/', getHomePage);
 router.post('/add-user', postAddUser);
-router.get('/create',getCreateUser);
+router.get('/create', getCreateUser);
+
+router.get('/edit/:id', editUser);
 
 module.exports = router;
