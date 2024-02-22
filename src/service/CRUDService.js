209add07bc433e8ec7b21deb33fd7ceb03537bfc
k1v4z <addnguyen32gmail.com>
2024-02-23 00:15:14 +0700

    connect database to update user

--- src/service/CRUDService.js
+++ src/service/CRUDService.js
@@ -16,12 +16,22 @@ const addUser = async (req) => {
 
     return results;
 }
+
 const getUserById = async (userId) => {
     const [results] = await connection.query('SELECT * FROM Users WHERE id = ?', [userId]);
     let user = results && results.length > 0 ? results[0] : {};
 
     return user;
 }
+
+const updateUser = async (email, name, city, id) => {
+    const [results] = await connection.query(`
+    UPDATE Users
+    SET email = ?, name = ?, city = ?
+    WHERE id = ?;
+    `, [email, name, city, id]);
+}
+
 module.exports = {
-    getAllUser, addUser, getUserById
+    getAllUser, addUser, getUserById, updateUser
 }
