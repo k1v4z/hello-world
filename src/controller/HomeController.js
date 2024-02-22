8ac0a9fa439f454f8b14f4f0382918e014edf5b4
k1v4z <addnguyen32gmail.com>
2024-02-22 23:00:12 +0700

    Add get user by id

--- src/controller/HomeController.js
+++ src/controller/HomeController.js
@@ -1,21 +1,28 @@
 const connection = require('../config/db');
-const { getAllUser, addUser } = require('../service/CRUDService');
+const { getAllUser, addUser, getUserById } = require('../service/CRUDService');
 
-const getHomePage = async (req, res) =>{
+const getHomePage = async (req, res) => {
     let results = await getAllUser();
-    return res.render('sample.ejs',{listUsers: results}); //object
+    return res.render('sample.ejs', { listUsers: results }); //object
 }
 
-const  postAddUser = async (req, res) =>{
+const postAddUser = async (req, res) => {
     //let {name, email, city} = req.body;
     const results = await addUser(req);
-    
+
     return res.send('add user succeed');
 }
 const getCreateUser = (req, res) => {
     return res.render('createuser.ejs');
 }
 
+const editUser = async (req, res) => {
+    let userId = req.params.id;
+    let user = await getUserById(userId);
+
+    return res.render('edituser.ejs', { editUser: user });
+}
+
 module.exports = {
-    getHomePage, postAddUser, getCreateUser //export object
+    getHomePage, postAddUser, getCreateUser, editUser //export object
 }
