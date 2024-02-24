00ccf66fa54074da50543da629bf55cb3f648fde
k1v4z <addnguyen32gmail.com>
2024-02-24 17:05:06 +0700

    add route

--- src/route/api.js
+++ src/route/api.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const {apigetUser, apigetUserById, apiDeleteUserById, apiCreateUser} = require('../controller/ApiController');
+const { apigetUser, apigetUserById, apiDeleteUserById, apiCreateUser, apiUpdateUser } = require('../controller/ApiController');
 
 let router = express.Router();
 
 const initApiRoute = (app) => {
     router.get('/users', apigetUser);
-    router.get('/users/:id',apigetUserById);
-    router.delete('/delete/users/:id',apiDeleteUserById);
-    router.post('/create',apiCreateUser);
-
+    router.get('/users/:id', apigetUserById);
+    router.delete('/delete/users/:id', apiDeleteUserById);
+    router.post('/create', apiCreateUser);
+    router.post('/update', apiUpdateUser);
+    
     return app.use('/api/v1/', router);
 }
 
