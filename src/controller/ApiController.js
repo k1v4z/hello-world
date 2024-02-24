e5b137df33b11ef2152c6774be9d031a62a8176b
k1v4z <addnguyen32gmail.com>
2024-02-24 17:04:22 +0700

    add apiUpdateUser

--- src/controller/ApiController.js
+++ src/controller/ApiController.js
@@ -1,4 +1,4 @@
-const { getAllUser, getUserById, destroyUser, addUser } = require("../service/CRUDService");
+const { getAllUser, getUserById, destroyUser, addUser, updateUser } = require("../service/CRUDService");
 
 const apigetUser = async (req, res) => {
     const allUser = await getAllUser();
@@ -34,12 +34,43 @@ const apiDeleteUserById = async (req, res) => {
 
 const apiCreateUser = async (req, res) => {
     const information = req.query;
+
     await addUser(information);
+
     return res.status(200).json({
         message: 'add user succeed'
     })
 }
 
+const apiUpdateUser = async (req, res) => {
+    const information = req.query;
+    let message = '';
+    const { email, name, city, id } = information;
+
+    const isExist = await getUserById(id);
+
+    if (isExist.id === undefined) {
+        message = 'user do not exist';
+
+        console.log(message);
+        return res.status(404).json({
+            message: message
+        });
+    }
+
+    if (!email || !name || !city) {
+        message = 'Please enter data';
+    } else {
+        await updateUser(email, name, city, id);
+
+        message = 'Update succeed';
+    }
+
+    return res.status(200).json({
+        message: message
+    })
+}
+
 module.exports = {
-    apigetUser, apigetUserById, apiDeleteUserById, apiCreateUser
+    apigetUser, apigetUserById, apiDeleteUserById, apiCreateUser, apiUpdateUser
 }
