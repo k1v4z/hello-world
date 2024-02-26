7082c228bbfa24bde40b29c6e893d915845959bc
k1v4z <addnguyen32gmail.com>
2024-02-26 23:34:34 +0700

    config sequelize

---
+++ src/config/Sequelize.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql', /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
+    port: 3307,
+    define: {
+        timestamps: false
+    }
+});
+
+module.exports = {
+    sequelize
+}
