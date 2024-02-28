eac274b1c8db6582df49f88c78ed7f7d11d84c3d
k1v4z <addnguyen32gmail.com>
2024-02-28 07:31:15 +0700

    handle error

--- src/route/web.js
+++ src/route/web.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+
 const { postAddUser, getHomePage, getCreateUser,
     editUser, postUpdateUser, postDeleteUser,
-    deleteUser } = require('../controller/HomeController');
+    deleteUser,
+    uploadFile,
+    uploadker,
+    uploadMultipleFile } = require('../controller/HomeController');
+
+let { upload, upload1 } = require('../config/multer');
 
 //router.Method(route,handler);
 router.get('/', getHomePage);
@@ -14,5 +21,20 @@ router.post('/update-user', postUpdateUser);
 
 router.post('/delete/user/:id', postDeleteUser);
 router.post('/delete-user', deleteUser);
+
+router.get('/upload', uploadFile);
+router.post('/upload-profile-pic', upload.single('profile_pic'), uploadker);
+router.post('/upload-multiple-images', (req, res, next) => {
+    // use middleware to handle error
+    upload1(req, res, (err) => {
+        if (err instanceof multer.MulterError && err.code === 'LIMIT_UNEXPECTED_FILE') {
+            res.send('LIMIT UNEXPECTED FILE');
+        } else if (err) {
+            res.send(err);
+        } else {
+            next();
+        }
+    })
+}, uploadMultipleFile);
 
 module.exports = router;
